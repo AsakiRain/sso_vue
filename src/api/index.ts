8cfad34c94f59cfca99bfb1c3936c503df6075e2
AsakiRain <1287059631@qq.com>
2023-02-09 04:17:52 +0800

    fix: 请求超时时的错误提示

--- src/api/index.ts
+++ src/api/index.ts
@@ -26,22 +26,28 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
 api.interceptors.response.use(
   (response: AxiosResponse) => {
     return response.data;
   },
   (error) => {
-    if (error.response.status.toString().startsWith(5)) {
-      // 若返回5开头的错误，服务器不一定有返回数据，所以额外判断
-      Message.error(error.response?.data.message || "服务器错误");
+    if (!error.response) {
+      console.log(error);
+      Message.error(error.message);
     } else {
-      // 对于其他状态码，服务器应当返回错误码和错误信息
-      Message.error(
-        `${error.response.data.code} ${error.response.data.message}`
-      );
-      if (error.response.status === 401) {
-        // 若返回401，说明是token相关错误，直接跳转到登录页
-        router.push("/login");
+      if (error.response.status.toString().startsWith(5)) {
+        // 若返回5开头的错误，服务器不一定有返回数据，所以额外判断
+        Message.error(error.response?.data.message || "服务器错误");
+      } else {
+        // 对于其他状态码，服务器应当返回错误码和错误信息
+        Message.error(
+          `${error.response.data.code} ${error.response.data.message}`
+        );
+        if (error.response.status === 401) {
+          // 若返回401，说明是token相关错误，直接跳转到登录页
+          router.push("/login");
+        }
       }
     }
     return Promise.reject(error);
